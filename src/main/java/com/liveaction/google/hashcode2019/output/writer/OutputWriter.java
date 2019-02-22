7ec0fad876b5780b3cd96918edc92227ac4aeffe
Jolan Cubells <jolan.cubells@gmail.com>
2019-02-22 14:42:25 +0100

    update writer for eneric google hash code

--- src/main/java/com/liveaction/google/hashcode2019/output/writer/OutputWriter.java
+++ src/main/java/com/liveaction/google/hashcode2019/output/writer/OutputWriter.java
@@ -1,6 +1,6 @@
 package com.liveaction.google.hashcode2019.output.writer;
 
-import com.liveaction.google.hashcode2019.model.Slice;
+import com.google.common.collect.Lists;
 
 import java.io.IOException;
 import java.nio.charset.Charset;
@@ -8,16 +8,13 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Collection;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class OutputWriter {
 
     public static final String OUTPUT_PATH = "output.out";
 
-    public void writeOutput(Collection<Slice> slices) throws IOException {
-        List<String> slicesAsString = slices.stream().map(slice -> String.format("%d %d %d %d", slice.r1, slice.c1, slice.r2, slice.c2)).collect(Collectors.toList());
-        slicesAsString.add(0, String.valueOf(slices.size()));
-        Files.write(Paths.get(OUTPUT_PATH), slicesAsString, Charset.defaultCharset());
+    public void writeOutput(Collection<?> inputs) throws IOException {
+        List<String> result = Lists.newArrayList(); // TODO
+        Files.write(Paths.get(OUTPUT_PATH), result, Charset.defaultCharset());
     }
-
 }
