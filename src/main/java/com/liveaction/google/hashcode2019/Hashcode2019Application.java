0ff0596b81f38ec54675301a5e35a7c742a4159c
Baptiste Le Bail <blebail@protnomail.com>
2019-02-22 13:39:24 +0100

    Add maven archetype

---
+++ src/main/java/com/liveaction/google/hashcode2019/Hashcode2019Application.java
@@ -0,0 +1,7 @@
+package com.liveaction.google.hashcode2019;
+
+public class Hashcode2019Application {
+
+	public static void main(String[] args) {
+	}
+}
---
+++ src/test/java/com/liveaction/google/hashcode2019/Hashcode2019ApplicationTest.java
@@ -0,0 +1,15 @@
+package com.liveaction.google.hashcode2019;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Hashcode2019ApplicationTest {
+
+    @Test
+    public void test() {
+        Hashcode2019Application hashcode2019Application = new Hashcode2019Application();
+
+        assertThat(hashcode2019Application).isNotNull();
+    }
+}
