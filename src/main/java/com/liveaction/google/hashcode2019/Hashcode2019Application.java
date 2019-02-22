704f185614594735a301787149486a6dcd4865bb
Baptiste Le Bail <blebail@protnomail.com>
2019-02-22 14:19:14 +0100

    Parse pizza input in main

--- src/main/java/com/liveaction/google/hashcode2019/Hashcode2019Application.java
+++ src/main/java/com/liveaction/google/hashcode2019/Hashcode2019Application.java
@@ -1,7 +1,17 @@
 package com.liveaction.google.hashcode2019;
 
+import com.liveaction.google.hashcode2019.input.PizzaInput;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Hashcode2019Application {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		String filePath = args[0];
+		Path path = Paths.get(filePath);
+
+		PizzaInput pizzaInput = PizzaInput.fromFile(path);
 	}
 }
