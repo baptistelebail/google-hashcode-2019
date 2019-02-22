559b171828b2d3ad292d42dd7252e94dbe0faa83
Jolan Cubells <jolan.cubells@gmail.com>
2019-02-22 14:21:01 +0100

    main

--- src/main/java/com/liveaction/google/hashcode2019/Hashcode2019Application.java
+++ src/main/java/com/liveaction/google/hashcode2019/Hashcode2019Application.java
@@ -1,10 +1,14 @@
 package com.liveaction.google.hashcode2019;
 
 import com.liveaction.google.hashcode2019.input.PizzaInput;
+import com.liveaction.google.hashcode2019.model.Slice;
+import com.liveaction.google.hashcode2019.model.SliceSolver;
+import com.liveaction.google.hashcode2019.output.writer.OutputWriter;
 
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Collection;
 
 public class Hashcode2019Application {
 
@@ -13,5 +17,11 @@ public class Hashcode2019Application {
 		Path path = Paths.get(filePath);
 
 		PizzaInput pizzaInput = PizzaInput.fromFile(path);
+
+		SliceSolver solver = null; // implementation
+
+		Collection<Slice> slices = solver.solve(pizzaInput.pizza, pizzaInput.minIngredients, pizzaInput.maxCells);
+
+		new OutputWriter().writeOutput(slices);
 	}
 }
