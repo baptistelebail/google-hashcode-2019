014055b25877b363a88724ef229dc8fe2ae0bb17
Baptiste Le Bail <blebail@protnomail.com>
2019-02-28 19:46:07 +0100

    fix test

--- src/test/java/com/liveaction/google/hashcode2019/input/InputTest.java
+++ src/test/java/com/liveaction/google/hashcode2019/input/InputTest.java
@@ -6,12 +6,10 @@ import java.net.URI;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import static org.junit.Assert.*;
-
 public class InputTest {
 
     @Test
-    public void fromFile() {
+    public void fromFile() throws Exception {
         Path pathToFile = Paths.get(new URI("a_example.txt"));
         final Input input = Input.fromFile(pathToFile);
 
