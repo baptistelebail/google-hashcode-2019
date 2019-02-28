a50a31fac10c16b56840f6568049262433c53055
Laurent Tonon <ltonon@liveaction.com>
2019-02-28 19:20:28 +0100

    input parser

--- src/main/java/com/liveaction/google/hashcode2019/input/Input.java
+++ src/main/java/com/liveaction/google/hashcode2019/input/Input.java
@@ -1,29 +1,55 @@
 package com.liveaction.google.hashcode2019.input;
 
+import com.google.common.collect.ImmutableSet;
+import com.liveaction.google.hashcode2019.model.Photo;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public final class Input {
 
-    public Input() {
+    private int photoNumber = 0;
+    private Set<Photo> photos;
+
+
+    private Input(int photoNumber, Set<Photo> photos) {
+        this.photoNumber = photoNumber;
+        this.photos = new HashSet<>(photos);
     }
 
     public static Input fromFile(Path path) throws IOException {
+        // Set of photos to pass to constructor
+        final Set<Photo> photos = new HashSet<>();
+
         List<String> lines = Files.readAllLines(path);
         int inputSize = lines.size();
 
         String header = lines.get(0);
-        String[] headerCells = header.split(" ");
+        int photoNumber = Integer.parseInt(header);
 
         for (int i = 1; i < inputSize; i++) {
+            int index = i - 1;
             String line = lines.get(i);
-
-            for (int j = 0; j < line.length(); j++) {
+            String[] lineCells = line.split(" ");
+            // get orientation
+            final String orientation = lineCells[0];
+            boolean horizontal = orientation.equals("H");
+            // get number of tags
+            final int numberOfTags = Integer.parseInt(lineCells[1]);
+            // set of tags
+            final ImmutableSet.Builder<String> builder = new ImmutableSet.Builder<String>();
+            for (int j = 2; j < lineCells.length; j++) {
+                builder.add(lineCells[j]);
             }
+
+            final Photo photo = new Photo(index, builder.build(), horizontal);
+            photos.add(photo);
         }
 
-        return new Input();
+        return new Input(photoNumber, photos);
     }
 }
