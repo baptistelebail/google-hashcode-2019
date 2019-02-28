4996f5298416b5265dceed16b3345c6438394160
Augustin Gjini <augustin.gjini@gmail.com>
2019-02-28 19:07:42 +0100

    score

--- src/main/java/com/liveaction/google/hashcode2019/model/Solver.java
+++ src/main/java/com/liveaction/google/hashcode2019/model/Solver.java
@@ -1,5 +1,7 @@
 package com.liveaction.google.hashcode2019.model;
 
+import com.google.common.collect.Sets;
+
 public class Solver {
 
     public void solve() {
@@ -7,7 +9,10 @@ public class Solver {
     }
 
     int score(Slide s1, Slide s2) {
-        s1.tags() - s2.tags();
+        Sets.SetView<String> intersection = Sets.intersection(s1.tags(), s2.tags());
+        Sets.SetView<String> difference1 = Sets.difference(s1.tags(), s2.tags());
+        Sets.SetView<String> difference2 = Sets.difference(s2.tags(), s1.tags());
+        return Math.min(intersection.size(), Math.min(difference1.size(), difference2.size()));
     }
 
 }
