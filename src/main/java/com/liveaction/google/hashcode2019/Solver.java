2991c166e47dd3e41eddf675380b7577e709df7a
Jolan Cubells <jcubells@liveaction.com>
2020-02-13 13:04:46 +0100

    naive solve

--- src/main/java/com/liveaction/google/hashcode2019/Solver.java
+++ src/main/java/com/liveaction/google/hashcode2019/Solver.java
@@ -2,10 +2,23 @@ package com.liveaction.google.hashcode2019;
 
 import com.liveaction.google.hashcode2019.file.manager.model.Input;
 import com.liveaction.google.hashcode2019.file.manager.model.Output;
+import it.unimi.dsi.fastutil.ints.IntArrayList;
+import it.unimi.dsi.fastutil.ints.IntList;
 
 public final class Solver {
 
     public Output solve(Input input) {
-        return null;
+        int total = 0;
+        int i = input.pizzaSizes.size();
+        IntList pizzasId = new IntArrayList(i);
+        while (total < input.maxSlices && i > 0) {
+            total += input.pizzaSizes.getInt(i);
+            pizzasId.add(i);
+            i--;
+        }
+        if(total > input.maxSlices) {
+            pizzasId.remove(pizzasId.size()-1);
+        }
+        return new Output(input, pizzasId);
     }
 }
--- src/main/java/com/liveaction/google/hashcode2019/file/manager/model/Input.java
+++ src/main/java/com/liveaction/google/hashcode2019/file/manager/model/Input.java
@@ -3,8 +3,8 @@ package com.liveaction.google.hashcode2019.file.manager.model;
 import it.unimi.dsi.fastutil.ints.IntList;
 
 public final class Input {
-    int maxSlices;
-    IntList pizzaSizes;
+    public final int maxSlices;
+    public final IntList pizzaSizes;
 
     public Input(int maxSlices, IntList pizzaSizes) {
         this.maxSlices = maxSlices;
