b87eed393876dc8907f669d4e25186ed2d383567
Jolan Cubells <jcubells@liveaction.com>
2020-02-13 12:49:49 +0100

    inputClass

--- src/main/java/com/liveaction/google/hashcode2019/file/manager/model/Input.java
+++ src/main/java/com/liveaction/google/hashcode2019/file/manager/model/Input.java
@@ -1,4 +1,8 @@
 package com.liveaction.google.hashcode2019.file.manager.model;
 
+import it.unimi.dsi.fastutil.ints.IntList;
+
 public final class Input {
+    int maxSlices;
+    IntList pizzaSize;
 }
