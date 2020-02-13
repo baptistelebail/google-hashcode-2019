d711403be0c5ac6b92abbb9806983810be74622f
Jolan Cubells <jcubells@liveaction.com>
2020-02-13 13:09:52 +0100

    fix

--- src/main/java/com/liveaction/google/hashcode2019/Solver.java
+++ src/main/java/com/liveaction/google/hashcode2019/Solver.java
@@ -9,7 +9,7 @@ public final class Solver {
 
     public Output solve(Input input) {
         int total = 0;
-        int i = input.pizzaSizes.size();
+        int i = input.pizzaSizes.size() -1;
         IntList pizzasId = new IntArrayList(i);
         while (total < input.maxSlices && i > 0) {
             total += input.pizzaSizes.getInt(i);
