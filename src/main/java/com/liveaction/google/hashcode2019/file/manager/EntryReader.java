76e4bb2689bd80225b576ecba1f3289b6abdba86
Jolan Cubells <jcubells@liveaction.com>
2020-02-13 12:54:31 +0100

    parse input

--- src/main/java/com/liveaction/google/hashcode2019/file/manager/EntryReader.java
+++ src/main/java/com/liveaction/google/hashcode2019/file/manager/EntryReader.java
@@ -1,12 +1,23 @@
 package com.liveaction.google.hashcode2019.file.manager;
 
 import com.liveaction.google.hashcode2019.file.manager.model.Input;
+import it.unimi.dsi.fastutil.ints.IntArrayList;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class EntryReader {
 
-    public Input getInput(File file) {
-        return null;
+    public Input getInput(File file) throws FileNotFoundException {
+        Scanner scanner = new Scanner(file);
+        int maxSlices = scanner.nextInt();
+        int nbPizzas = scanner.nextInt();
+        String[] sizes = scanner.nextLine().split(" ");
+        IntArrayList pizzaSizes = new IntArrayList(nbPizzas);
+        for (String s : sizes) {
+            pizzaSizes.add(Integer.parseInt(s));
+        }
+        return new Input(maxSlices, pizzaSizes);
     }
 }
--- src/main/java/com/liveaction/google/hashcode2019/file/manager/model/Input.java
+++ src/main/java/com/liveaction/google/hashcode2019/file/manager/model/Input.java
@@ -4,5 +4,10 @@ import it.unimi.dsi.fastutil.ints.IntList;
 
 public final class Input {
     int maxSlices;
-    IntList pizzaSize;
+    IntList pizzaSizes;
+
+    public Input(int maxSlices, IntList pizzaSizes) {
+        this.maxSlices = maxSlices;
+        this.pizzaSizes = pizzaSizes;
+    }
 }
