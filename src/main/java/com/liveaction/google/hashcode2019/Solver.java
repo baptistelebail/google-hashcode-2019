fc96c698e82a3c0eef687b76b899367ded31039f
Jolan Cubells <jcubells@liveaction.com>
2020-02-13 12:45:47 +0100

    wip

---
+++ src/main/java/com/liveaction/google/hashcode2019/Solver.java
@@ -0,0 +1,11 @@
+package com.liveaction.google.hashcode2019;
+
+import com.liveaction.google.hashcode2019.file.manager.model.Input;
+import com.liveaction.google.hashcode2019.file.manager.model.Output;
+
+public final class Solver {
+
+    public Output solve(Input input) {
+        return null;
+    }
+}
