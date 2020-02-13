007851838797913a461b6a166e9a01fd0e9d8e25
Jolan Cubells <jcubells@liveaction.com>
2020-02-13 12:43:27 +0100

    wip

--- src/main/java/com/liveaction/google/hashcode2019/file/manager/EntryReader.java
+++ src/main/java/com/liveaction/google/hashcode2019/file/manager/EntryReader.java
@@ -1,4 +1,12 @@
 package com.liveaction.google.hashcode2019.file.manager;
 
+import com.liveaction.google.hashcode2019.file.manager.model.Input;
+
+import java.io.File;
+
 public class EntryReader {
+
+    public Input getInput(File file) {
+        return null;
+    }
 }
--- src/main/java/com/liveaction/google/hashcode2019/file/manager/OutputWriter.java
+++ src/main/java/com/liveaction/google/hashcode2019/file/manager/OutputWriter.java
@@ -1,4 +1,10 @@
 package com.liveaction.google.hashcode2019.file.manager;
 
+import com.liveaction.google.hashcode2019.file.manager.model.Output;
+
 public class OutputWriter {
+
+    public void writeResult(Output output) {
+
+    }
 }
