26da587b25288e044897b7730e91abe95a86c7ca
Simon Daudin <sdaudin@liveaction.com>
2020-02-20 19:13:10 +0100

    add outputwriter and test

--- src/main/java/com/liveaction/google/hashcode2019/file/manager/OutputWriter.java
+++ src/main/java/com/liveaction/google/hashcode2019/file/manager/OutputWriter.java
@@ -1,6 +1,8 @@
 package com.liveaction.google.hashcode2019.file.manager;
 
+import com.google.common.base.Joiner;
 import com.liveaction.google.hashcode2019.file.manager.model.Output;
+import it.unimi.dsi.fastutil.ints.IntList;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -20,5 +22,12 @@ public class OutputWriter {
     }
 
     private void writeResult(Output output, Path dest) throws IOException {
+        Files.write(dest, Integer.toString(output.booksPerLibrary.size()).getBytes());
+        for (Integer library : output.libraries) {
+            IntList books = output.booksPerLibrary.get(library);
+            Files.write(dest, String.format("%d %d", library, books.size()).getBytes());
+            String join = Joiner.on(" ").join(books);
+            Files.write(dest, join.getBytes());
+        }
     }
 }
--- src/test/java/com/liveaction/google/hashcode2019/Hashcode2019ApplicationTest.java
+++ src/test/java/com/liveaction/google/hashcode2019/Hashcode2019ApplicationTest.java
@@ -1,25 +1,32 @@
 package com.liveaction.google.hashcode2019;
 
+import com.google.common.collect.ImmutableList;
+import com.liveaction.google.hashcode2019.file.manager.EntryReader;
+import com.liveaction.google.hashcode2019.file.manager.OutputWriter;
+import com.liveaction.google.hashcode2019.file.manager.model.Input;
+import com.liveaction.google.hashcode2019.file.manager.model.Output;
 import org.junit.Test;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Path;
 
 public class Hashcode2019ApplicationTest {
 
-    private final static String A = "src/test/resources/a_example.in";
-    private final static String B = "src/test/resources/b_small.in";
-    private final static String C = "src/test/resources/c_medium.in";
-    private final static String D = "src/test/resources/d_quite_big.in";
-    private final static String E = "src/test/resources/e_also_big.in";
+    private final static String A = "src/test/resources/a_example.txt";
+    private final static String B = "src/test/resources/b_read_on.txt";
+    private final static String C = "src/test/resources/c_incunabula.txt";
+    private final static String D = "src/test/resources/d_tough_choices.txt";
+    private final static String E = "src/test/resources/e_so_many_books.txt";
 
     @Test
     public void test() throws IOException {
-//        for (String file : ImmutableList.of(A, B, C, D, E)) {
-//            Input input = new EntryReader().getInput(new File(file));
-//            Output solve = new Solver().solve(input);
-//            System.out.println("score : " + solve.score());
-//            Path dest = new OutputWriter().writeResultInFile(solve);
-//            System.out.println("dest : " + dest);
-//        }
+        for (String file : ImmutableList.of(A, B, C, D, E)) {
+            Input input = new EntryReader().getInput(new File(file));
+            Output solve = new Solver().solve(input);
+            System.out.println("score : " + solve.score());
+            Path dest = new OutputWriter().writeResultInFile(solve);
+            System.out.println("dest : " + dest);
+        }
     }
 }
