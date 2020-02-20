95e0c982ae045fffcc5eeac2b48985504180b817
Jolan Cubells <jcubells@liveaction.com>
2020-02-20 19:32:00 +0100

    start less naive solver

--- src/main/java/com/liveaction/google/hashcode2019/Solver.java
+++ src/main/java/com/liveaction/google/hashcode2019/Solver.java
@@ -2,11 +2,16 @@ package com.liveaction.google.hashcode2019;
 
 import com.google.common.collect.Maps;
 import com.liveaction.google.hashcode2019.file.manager.model.Input;
+import com.liveaction.google.hashcode2019.file.manager.model.Library;
 import com.liveaction.google.hashcode2019.file.manager.model.Output;
 import it.unimi.dsi.fastutil.ints.IntArrayList;
 import it.unimi.dsi.fastutil.ints.IntList;
 
+import java.util.Comparator;
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 public final class Solver {
 
@@ -22,5 +27,23 @@ public final class Solver {
         }
 
         return new Output(input, libraries, booksPerLibrary);
+    }
+
+    private Optional<Library> bestLibrary(List<Library> libraries, int daysRemaining, int[] books) {
+        return libraries.stream().max(Comparator.comparingInt(l -> scoreLibrary(l, daysRemaining, books)));
+    }
+
+    private int scoreLibrary(Library library, int daysRemaining, int[] books) {
+        int realDays = daysRemaining - library.signupDays;
+        int nbBooks = realDays * library.parellelScanning;
+        List<Integer> orderedBooks = library.books
+                .stream()
+                .sorted(Comparator.comparingInt(b -> -books[b]))
+                .collect(Collectors.toList());
+        return orderedBooks.subList(0, nbBooks)
+                .stream()
+                .reduce((i, j) -> i+j)
+                .orElse(0);
+
     }
 }
