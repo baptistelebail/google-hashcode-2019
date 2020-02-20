058b8c9a9845fb83781e72b7130b39edf0c0c3fd
Jolan Cubells <jcubells@liveaction.com>
2020-02-20 20:07:02 +0100

    newSolver

--- src/main/java/com/liveaction/google/hashcode2019/Solver.java
+++ src/main/java/com/liveaction/google/hashcode2019/Solver.java
@@ -1,5 +1,7 @@
 package com.liveaction.google.hashcode2019;
 
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.liveaction.google.hashcode2019.file.manager.model.Input;
 import com.liveaction.google.hashcode2019.file.manager.model.Library;
@@ -19,11 +21,31 @@ public final class Solver {
         IntList libraries = new IntArrayList(input.libraries.size());
         Map<Integer, IntList> booksPerLibrary = Maps.newHashMap();
 
+        List<Library> libsToParse = Lists.newArrayList(input.libraries);
         int days = input.nbDays;
 
-
-
-
+        while(libsToParse.size() > 0) {
+            List<Library> libsToRemove = Lists.newArrayList();
+            for(Library lib : libsToParse) {
+                if (lib.signupDays <= days) {
+                    libsToRemove.add(lib);
+                }
+            }
+            libsToParse.removeAll(libsToRemove);
+            Library bestLib = bestLibrary(libsToParse, days, input.books).orElse(null);
+            if(bestLib == null)            {
+                System.out.println("ERROR : bestLib should not be null");
+                break;
+            }
+            days = days - bestLib.signupDays;
+            libsToParse.remove(bestLib);
+            libraries.add(input.libraries.indexOf(bestLib));
+            List<Integer> collect = bestLib.books.stream()
+                    .sorted(Comparator.comparingInt(b -> -input.books[b]))
+                    .collect(Collectors.toList());
+            booksPerLibrary.put(input.libraries.indexOf(bestLib), new IntArrayList(collect));
+        }
+        return new Output(input, libraries, booksPerLibrary);
     }
 
     private Optional<Library> bestLibrary(List<Library> libraries, int daysRemaining, int[] books) {
