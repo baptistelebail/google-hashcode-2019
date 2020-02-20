00f2d89b6e67edc3f70c3ce09dd7499f6e5e1bbc
Laurent Tonon <ltonon@liveaction.com>
2020-02-20 19:21:16 +0100

    WIP: pushing entry reader

--- src/main/java/com/liveaction/google/hashcode2019/file/manager/EntryReader.java
+++ src/main/java/com/liveaction/google/hashcode2019/file/manager/EntryReader.java
@@ -1,16 +1,54 @@
 package com.liveaction.google.hashcode2019.file.manager;
 
 import com.liveaction.google.hashcode2019.file.manager.model.Input;
+import com.liveaction.google.hashcode2019.file.manager.model.Library;
+import it.unimi.dsi.fastutil.ints.IntArrayList;
+import it.unimi.dsi.fastutil.ints.IntList;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class EntryReader {
 
     public Input getInput(File file) throws FileNotFoundException {
-        Scanner scanner = new Scanner(file);
+        // create libraries first
+        List<Library> libraries = new ArrayList<>();
 
-        return null;
+        String name = file.getName();
+        Scanner scanner = new Scanner(file);
+        // Parse first line nbBooks & nbLibrary
+        int nbBooks = scanner.nextInt();
+        int nbLibraries = scanner.nextInt();
+        int nbDays = scanner.nextInt();
+        // Next line: get score of different books
+        scanner.nextLine();
+        IntList bookScore = new IntArrayList();
+        for(int i=0; i<nbBooks; i++) {
+            bookScore.set(i, scanner.nextInt());
+        }
+        // Parse libraries informations
+        for(int i=0; i<nbLibraries; i++) {
+            // First line:
+            scanner.nextLine();
+            int numberOfBooks = scanner.nextInt();
+            int numberOfDaysForSignup = scanner.nextInt();
+            int shippingPerDay = scanner.nextInt();
+            // Second line
+            String bookIdsLine = scanner.nextLine();
+            IntList bookIds = new IntArrayList();
+            String[] strBookIds = bookIdsLine.split(" ");
+            for (int j = 0; j < strBookIds.length; j++) {
+                bookIds.set(j, new Integer(strBookIds[j]).intValue());
+            }
+
+            Library library = new Library(numberOfDaysForSignup, shippingPerDay, bookIds);
+            libraries.add(library);
+        }
+
+        Input input = new Input(name, null, libraries, nbDays);
+        return input;
     }
 }
